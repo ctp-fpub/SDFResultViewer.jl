eefcdf14d7927c87ccd4070bb51863044a6b7fc1
Sebastian Micluța-Câmpeanu <m.c.sebastian95@gmail.com>
2021-01-23 17:16:56 +0200

    Add λ based units

---
+++ src/units.jl
@@ -0,0 +1,20 @@
+function add_λ_units(λ)
+    expr = quote
+
+        using Unitful
+        using PhysicalConstants.CODATA2018: c_0, ε_0, m_e, e
+
+        local λ = $λ
+        local ω = 2π*c_0/λ
+
+        @unit unit_l "c/ω" UnitLength c_0/ω false
+        @unit unit_t "1/ω" UnitTime 1/ω false
+        @unit unit_B "mₑ ω/e" UnitMagneticField m_e * ω / e false
+        @unit unit_E "mₑ c ω/e" UnitElectricField m_e * ω / e * c_0 false
+        @unit unit_L "mₑ c²/ω" UnitAngularMomenta m_e * c_0^2 / ω false
+        @unit pₑ "mₑ c" ElectronMomenta m_e * c_0 false
+
+        Unitful.register(@__MODULE__)
+    end
+    eval(expr)
+end
