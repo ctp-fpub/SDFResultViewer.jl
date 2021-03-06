ba882e7e9a6d1365dd783b4300de624bb6aeb14b
Sebastian Micluța-Câmpeanu <m.c.sebastian95@gmail.com>
2021-03-06 16:31:28 +0200

    Move analysis code to PICAnalysisTools.jl

--- src/SDFResultViewer.jl
+++ src/SDFResultViewer.jl
@@ -1,27 +1,19 @@
 module SDFResultViewer
 
-export compute_L, compute_Lx,
-    mean_quantity, approximate_field,
-    add_λ_units, unit_l, unit_t, unit_E, unit_B, unit_L, pₑ,
+export add_λ_units, unit_l, unit_t, unit_E, unit_B, unit_L, pₑ,
     average_linear_density_x, npart_plot,
     scalar_field_widget
 
 using SDFResults
 using PICDataStructures
 using PICDataStructures: dir_to_idx
-using RecursiveArrayTools
-using LinearAlgebra
+using PICAnalysisTools
 using Unitful
 using PhysicalConstants.CODATA2018: c_0, ε_0, m_e, e
-using StaticArrays
-using FileTrees
-using NumericalIntegration
-using Statistics
-using Transducers, ThreadsX
+import Plots
+using UnitfulRecipes
 
 include("units.jl")
-include("analysis.jl")
-include("number_density.jl")
 include("plots.jl")
 include("widgets.jl")
 
