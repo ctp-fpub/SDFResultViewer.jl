7a9f079856cce51d1a1226bc552bfb3b9fed604e
Mircea Dolineanu <mircea_dolineanu@yahoo.com>
2021-03-27 12:14:26 +0200

    remove comented section
    Co-authored-by: Sebastian Micluța-Câmpeanu <31181429+SebastianM-C@users.noreply.github.com>

--- test/runtests.jl
+++ test/runtests.jl
@@ -32,16 +32,4 @@ using Test
     
     @test SDFResultViewer.cell_length(sim, :x) == 15u"μm"/450
     
-    # λ = 800u"nm"
-    # dir = pwd()
-    # file = "0002.sdf"
-    # times = get_times(dir, λ)
-    # @test length(times) == 1
-
-    # m_Lx = mean_Lx(dir, λ)
-    # @test length(m_Lx) == 1
-
-    # Lx, y, z = Lx_slice(joinpath(dir, file), λ, 0)
-    # @test length(Lx) > 0
-    # @test length(Lx) == length(y) == length(z)
 end
