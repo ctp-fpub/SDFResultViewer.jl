29f21af4958e7e29e3615dd276b6362fe6043937
Dolineanu Mircea <mircea_dolineanu@yahoo.com>
2021-03-27 09:28:28 +0200

    new methods

--- src/plots.jl
+++ src/plots.jl
@@ -214,6 +214,65 @@ function photon_energy_spectrum(sim; label = "label", mark_max = false, angle = 
     return plt
 end
 
+# an alternative version of photon_energy_spectrum() 
+# that plots multiple spectra on the same plot
+function photon_energy_spectrum(sims, labels; legend_title = "labels", mark_max = false, angle = pi, species = "photon", plot_title = "Compton scattering spectrum")
+    kwargs = (xlabel = "sᵧ = log₁₀(Eᵧ/keV)", 
+            ylabel = "sᵧ² dN/dsᵧ", 
+            tickfontsize = 10, 
+            guidefontsize = 15, 
+            title = plot_title,
+            legendfontsize = 15, 
+            legendtitlefont = 15,
+            titlefont = 16,
+            legendtitle = legend_title
+    )
+    plt = Plots.plot(;kwargs...)
+    for (i, sim) in enumerate(sims)
+        file = sim[end]
+        px, py, pz = file["px/$species", "py/$species", "pz/$species"]
+        # filtering data: keep only photons moving in the direction of the laser
+        idx = findall(p -> p > zero(eltype(px)), px)
+        px = px[idx]
+        py = py[idx]
+        pz = pz[idx]
+
+        # θ is measured from the x axis
+        # filtering data: keep photons that propagate within 2θ
+        pₚₑᵣₚ = sqrt.(py.^2 + pz.^2)   #transversal momentum of each photon
+        θ = atan.(pₚₑᵣₚ./px)           #emission angles for each photon
+        idx = findall(θ₀ -> θ₀ < angle/2, θ)
+        px = px[idx]
+        py = py[idx]
+        pz = pz[idx]
+
+        # make plot
+        Eᵧ = sqrt.(px.^2 + py.^2 + pz.^2) * c_0
+        sᵧ = log.(ustrip(u"keV", Eᵧ))
+
+        h = fit(Histogram, sᵧ; nbins = 200)
+        s = midpoints(collect(h.edges[1]))
+        N = h.weights
+        dNds = N./abs(s[1]-s[2]) 
+
+        idx = findall(x -> x > 0, s)
+        s = s[idx]
+        dNds = dNds[idx]
+        plt = Plots.plot!(plt, s, dNds.*s.*s, linewidth = 4; label = labels[i])
+
+        if mark_max
+            dNds = dNds.*s.*s
+            p0 = findfirst(smax -> smax == maximum(dNds), dNds)
+            Plots.plot!(plt, [s[p0]], [dNds[p0]], seriestype = :scatter, 
+                                    markersize = 7, 
+                                    markercolor = :black,
+                                    label = false
+            )
+        end
+    end
+    return plt
+end
+
 @doc """
 Energy spectra s² dN/ds = f(s = log₁₀(E/MeV)) for particles with mass.
 The mass of the particle has to be added by hand with the mass argument (default is the electron mass).
@@ -247,6 +306,42 @@ function energy_spectrum(sim; mass = m_e, species = "electron", plot_title = "El
     
     plt = Plots.plot(s, dNds.*s.*s, linewidth = 4; kwargs...)
 
+    return plt
+end
+
+# an alternative version of energy_spectrum() 
+# that plots multiple spectra on the same plot
+function energy_spectrum(sims, labels; legend_title = "labels", mass = m_e, species = "electron", plot_title = "Electron energy spectrum")
+    kwargs = (xlabel = "s = log₁₀(E/MeV)", 
+              ylabel = "s² dN/ds", 
+              tickfontsize = 10, 
+              guidefontsize = 15, 
+              title = plot_title,
+              legendfontsize = 15, 
+              legendtitlefont = 15,
+              titlefont = 16,
+              legendtitle = legend_title
+    )
+    plt = Plots.plot(;kwargs...)
+    for (i, sim) in enumerate(sims)
+        file = sim[end]
+        px, py, pz = file["px/$species", "py/$species", "pz/$species"]
+
+        #should be implemented: read species mass from input.deck
+        E₀ = sqrt.(mass^2*c_0^4 .+ (px.^2 + py.^2 + pz.^2) * c_0^2)
+        s₀ = log.(ustrip(u"MeV", E₀))
+
+        h = fit(Histogram, s₀; nbins = 200)
+        s = midpoints(collect(h.edges[1]))
+        N = h.weights
+        dNds = N./abs(s[1]-s[2]) 
+
+        idx = findall(x -> x > 0, s)
+        s = s[idx]
+        dNds = dNds[idx]
+    
+        plt = Plots.plot!(plt, s, dNds.*s.*s, linewidth = 4; label = labels[i])
+    end
     return plt
 end
 
