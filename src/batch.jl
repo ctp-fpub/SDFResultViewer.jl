1bdb74cfe148600e0a50c90f80f954ac5e6e3449
Sebastian Micluța-Câmpeanu <m.c.sebastian95@gmail.com>
2021-04-05 17:16:26 +0300

    Improve batch plotting

--- src/batch.jl
+++ src/batch.jl
@@ -14,14 +14,32 @@ function batch_plot(sim::EPOCHSimulation, f, folder_name, file_name, args...; ov
                 continue
             end
         end
+        plt = f(file, args...; kwargs...)
+        isnothing(plt) && continue
+        Plots.savefig(plt, fn)
+    end
+end
+
+function batch_plot(sims, f, file_name::Function, args...; overwrite=true, kwargs...)
+    @progress name="Batch plot" for sim in sims
+        fn = joinpath(sim.dir, file_name(sim))
+        if isfile(fn)
+            @info "File $fn already exists"
+            if overwrite
+                @debug "Overwriting $fn..."
+            else
+                @debug "Skipping"
+                continue
+            end
+        end
         plt = f(sim, args...; kwargs...)
         isnothing(plt) && continue
         Plots.savefig(plt, fn)
     end
 end
 
-function batch_plot(sims, args...; kwargs...)
-    @progress for sim in sims
-        batch_plot(sim, args...; kwargs...)
+function batch_plot(sims, f, folder_name, file_name::Function, args...; kwargs...)
+    @progress name="Batch plot" for sim in sims
+        batch_plot(sim, f, folder_name, file_name, args...; kwargs...)
     end
 end
