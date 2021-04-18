54f08099cee042679324befb38f4c0805fea7314
Sebastian Micluța-Câmpeanu <m.c.sebastian95@gmail.com>
2021-04-18 23:31:38 +0300

    Refactor analytic comparisons

--- src/plots/fields.jl
+++ src/plots/fields.jl
@@ -10,24 +10,99 @@ function E_slice_plot(E::ScalarField, dir, slice_location, t;
         E_slice = E
     end
 
-    grid = getdomain(E_slice)
-    cl = ustrip(max(abs.(extrema(E_slice))...))
-    ts = sprint(show, t, context=:compact => true)
-    loc = sprint(show, slice_location, context=:compact => true)
-    xlabel, ylabel = filter(x->x≠string(slice_dir), ["x", "y", "z"])
+function compare_slice_with_analytic(numeric_f, f, laser, profile, file;
+    component=1,
+    slice_location=0unit_l,
+    slice_dir=:x,
+    label="",
+    f_unit)
 
-    plt = Plots.heatmap(grid..., E_slice;
-        title = "E$dir at t=$ts, x=$loc",
-        xlabel, ylabel, colorbar_title = "E$dir",
-        seriescolor = :jet1,
-        aspect_ratio = 1,
-        framestyle = :box,
-        clims = (-cl, cl),
-        kwargs...)
+    fx = get_parameter(file, :constant, :f_x)*u"m" |> unit_l
+    t = get_time(file) |> unit_t
+    s = analytic_laser(laser, profile, file)
 
-    if save
-        save && Plots.savefig(plt, "E$(dir_slice)_$loc.png")
-    end
+    grid = getdomain(numeric_f)
+
+    analytic_f = apply_analytic(f, grid, t, s;
+        component,
+        propagation_dir=:x,
+        x₀=fx) |> f_unit
+
+    fig = Figure(resolution = (800, 700))
+
+    units = " (" * string(unit(recursive_bottom_eltype(grid))) * ")"
+    xlabel, ylabel = filter(x->x≠string(slice_dir),
+        ["x", "y", "z"] .* units)
+    # ts = @lift sprint(show, $t, context=:compact => true)
+    # loc = @lift sprint(show, $slice_location, context=:compact => true)
+
+    ax1 = Axis(fig[1,1][1,1];
+        xlabel, ylabel,
+        title = "Analytic",
+        aspect = DataAspect()
+    )
+    ax2 = Axis(fig[1,2][1,1];
+        xlabel, ylabel,
+        title = "Numeric",
+        aspect = DataAspect()
+    )
+
+    analytic_slice, loc = to_observable(analytic_f; slice_dir, slice_location, issliced=false)
+    numeric_slice, loc = to_observable(numeric_f; slice_dir, slice_location, issliced=false)
+
+    plt1 = fieldplot!(ax1, analytic_slice)
+    plt2 = fieldplot!(ax2, numeric_slice)
+
+    Colorbar(fig[1,1][2,1], plt1;
+        width = Relative(3/4),
+        height = 10,
+        vertical = false,
+        tellheight = true,
+        label,
+    )
+    Colorbar(fig[1,2][2,1], plt2;
+        width = Relative(3/4),
+        height = 10,
+        vertical = false,
+        tellheight = true,
+        label,
+    )
+
+    return fig
+end
+
+function compare_E_slice_with_analytic(laser, profile, file, dir;
+    slice_location=0.0unit_l,
+    slice_dir=:x)
+
+    E = uconvert(unit_E, unit_l, file["e$dir"])
+
+    compare_slice_with_analytic(E, LaserTypes.E, laser, profile, file;
+        component=dir_to_idx(dir),
+        slice_location,
+        slice_dir,
+        f_unit=unit_E,
+    )
+end
+
+function compare_S_slice_with_analytic(laser, profile, file, dir;
+    slice_location=0.0unit_l,
+    slice_dir=:x)
+
+    Ex, Ey, Ez = uconvert.(unit_E, unit_l, file[:ex, :ey, :ez])
+    Bx, By, Bz = uconvert.(unit_B, unit_l, file[:bx, :by, :bz])
+
+    E = build_vector((Ex, Ey, Ez), (:x, :y, :z))
+    B = build_vector((Bx, By, Bz), (:x, :y, :z))
+    S = ustrip(E × B) / ustrip(μ_0)
+
+    compare_slice_with_analytic(S, LaserTypes.S, laser, profile, file;
+        component=dir_to_idx(dir),
+        slice_location=ustrip(slice_location),
+        slice_dir,
+        f_unit=unit_E,
+    )
+end
 
     return plt
 end
